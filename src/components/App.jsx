61b85708355eb18cd9d6eb36f1a2eef36437b0dd
Vladimir Tsymbaliuk <89538231+RichardDitrix@users.noreply.github.com>
2024-02-04 20:20:39 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import ContactForm from './components/ContactForm';
-import ContactList from './components/ContactList';
-import Filter from './components/Filter';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
 
 
 export const App = () => {
